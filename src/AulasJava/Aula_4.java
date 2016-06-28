3e94578ced9e33f87e941506cedddbfa46d1b28d
danilosilvaaugusto <agendasigepe@gmail.com>
2016-06-28 18:49:18 -0300

    teste

---
+++ src/AulasJava/Aula_4.java
@@ -0,0 +1,14 @@
+package AulasJava;
+
+public class Aula_4 {
+
+	public static void main (String []args){
+
+		//variavél 
+		int num;
+		num = 150;
+		
+		//Chamando uma variavél para aparecer em um print
+		System.out.println("Você me deve " + num + " me devendo");
+	}
+}
